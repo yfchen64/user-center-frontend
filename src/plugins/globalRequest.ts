5a0972fa5c90483e6beca8ffdefc2f8ad14d5854
chen <cyxwork64@163.com>
2023-03-22 17:29:07 +0800

    add Dockerfile add nginx.conf

--- src/plugins/globalRequest.ts
+++ src/plugins/globalRequest.ts
@@ -13,6 +13,7 @@ import { message } from 'antd';
  */
 const request = extend({
   credentials: 'include', // 默认请求是否带上cookie
+  prefix: process.env.NODE_ENV === 'production' ? 'http://user-backend.code_nav.cn' : undefined
 });
 
 /**
